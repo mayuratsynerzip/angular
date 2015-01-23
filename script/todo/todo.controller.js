64a652a3b6dba2d31dff7c680e91a5cce592af54
unknown <mayurk@ULTP_375.synerzipune.local>
2015-01-23 18:12:28 +0530

    added todo module

---
+++ script/todo/todo.controller.js
@@ -0,0 +1,31 @@
+/**
+ * Created by mayurk on 1/23/2015.
+ */
+angular.module("todo")
+.controller("MainCtrl",["$scope","TodoService", function($scope, TodoService){
+    $scope.todos = TodoService.todos;
+
+    $scope.add = function(){
+        TodoService.add($scope.newTodos);
+        $scope.newTodos = {};
+    }
+    /*
+     $scope.keypress = function() {
+     if(event.keyCode == 33) {
+     $scope.add();
+     }
+     }*/
+    $scope.delete = function(item) {
+        var index = $scope.todos.indexOf(item);
+        $scope.todos.splice(index,1);
+    }
+    $scope.clearAll = function(){
+        $scope.todos = [];
+    }
+    $scope.clearCompleted = function () {
+        TodoService.clearCompleted();
+    }
+    $scope.clearAll = function () {
+        TodoService.clearAll();
+    }
+}]);
---
+++ script/todo/todo.services.js
@@ -0,0 +1,26 @@
+/**
+ * Created by mayurk on 1/23/2015.
+ */
+angular.module("todo")
+.service("TodoService",[function () {
+    return {
+        todos:[],
+        add:function(item) {
+            console.log(item);
+            this.todos.push(item);
+        },
+        delete:function(item){
+            this.todos.splice(this.todos.indexOf(item));
+        },
+        clearCompleted: function(){
+            for(var i=0; i<this.todos.length;i++) {
+                if(this.todos[i].completed == true) {
+                    this.todos.splice(i,1);
+                }
+            }
+        },
+        clearAll: function(){
+            this.todos = [];
+        }
+    }
+}]);
